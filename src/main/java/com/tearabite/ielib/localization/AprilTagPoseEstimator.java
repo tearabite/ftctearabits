038974fff032b843beda7da60c31d08ec700c69e
Scott Barnes <scott@tearabite.com>
2024-03-08 06:24:50 -0600

    fdfddfd

--- src/main/java/com/tearabite/ielib/localization/AprilTagPoseEstimator.java
+++ src/main/java/com/tearabite/ielib/localization/AprilTagPoseEstimator.java
@@ -1,13 +1,7 @@
 package com.tearabite.ielib.localization;
 
-import static java.lang.Math.PI;
-import static java.lang.Math.asin;
-import static java.lang.Math.atan2;
-import static java.lang.Math.cos;
-import static java.lang.Math.pow;
-import static java.lang.Math.sin;
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.tearabite.ielib.localization.AprilTagPoseEstimatorCore;
 
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
@@ -16,16 +10,12 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 
 import java.security.InvalidParameterException;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
-public class AprilTagPoseEstimator {
+@Getter
+@SuperBuilder(toBuilder=true)
+public class AprilTagPoseEstimator extends AprilTagPoseEstimatorCore {
 
     /*
     * This class is used to estimate the pose of the robot using the AprilTagDetection object.
@@ -34,8 +24,6 @@ public class AprilTagPoseEstimator {
     * The math done hre can be visualized using the following Desmos graph:
     * https://www.desmos.com/calculator/n2iyatwssg
      */
-
-    @Getter @Setter @Builder.Default  private Pose2d robotOffset = new Pose2d(0,0,0);
 
     /**
      * Estimates the pose of the robot using the AprilTagDetection object.
@@ -60,46 +48,5 @@ public class AprilTagPoseEstimator {
                 fieldOrientation.y,
                 fieldOrientation.z,
                 fieldOrientation.w);
-    }
-
-    /**
-     * Estimates the pose of the robot using the AprilTagDetection object.
-     * @param yaw The rotation about Z of the AprilTag within the camera's frame.
-     * @param bearing The angle between line projected to center of the april tag and the camera's center projection line.
-     * @param range The distance from the center of the camera lens to the center of the AprilTag.
-     * @param Tx The x position (in FTC field coordinates) of the AprilTag
-     * @param Ty The y position (in FTC field coordinates) of the AprilTag
-     * @param Ta The a component of the quaternion describing the orientation of the AprilTag
-     * @param Tb The b component of the quaternion describing the orientation of the AprilTag
-     * @param Tc The c component of the quaternion describing the orientation of the AprilTag
-     * @param Td The d component of the quaternion describing the orientation of the AprilTag
-     * @return The estimated pose of the robot. Heading is reported in radians.
-     */
-    private Pose2d estimatePose(double yaw, double bearing, double range, double Tx, double Ty, double Ta, double Tb, double Tc, double Td) {
-        double Tyaw = getTYaw(Ta, Tb, Tc, Td);
-        double ch = (PI / 2) - Tyaw + yaw;
-        double cx = Tx + range * cos(bearing) * cos(ch) + range * sin(bearing) * sin(ch);
-        double cy = Ty + range * cos(bearing) * sin(ch) - range * sin(bearing) * cos(ch);
-
-        double offsetX = this.robotOffset.getX();
-        double offsetY = this.robotOffset.getY();
-        double Cyaw = this.robotOffset.getHeading();
-        double rh = (PI / 2) + Tyaw + yaw - Cyaw;
-        double rx = cx + offsetX * cos(rh) - offsetY * sin(rh);
-        double ry = cy + offsetX * sin(rh) + offsetY * cos(rh);
-
-        return new Pose2d(rx, ry, rh);
-    }
-
-    private static double getTPitch(double a, double b, double c, double d) {
-        return asin(2 * ((a * d) - (b * c)));
-    }
-
-    private static double getTRoll(double a, double b, double c, double d) {
-        return atan2(2 * ((a * c) + (b * d)), 1 - 2 * (pow(c, 2) + pow(d, 2)));
-    }
-
-    private static double getTYaw(double a, double b, double c, double d) {
-        return atan2(2 * ((a * b) + (c * d)), 1 - 2 * ((pow(b, 2) + pow(c, 2))));
     }
 }
